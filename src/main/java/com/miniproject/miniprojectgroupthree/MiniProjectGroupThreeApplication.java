c66ffee9850082bca95e86258e44aafbf0138cce
Irang <irang0426@gmail.com>
2023-03-17 13:49:34 +0900

    first commit

---
+++ src/main/java/com/miniproject/miniprojectgroupthree/MiniProjectGroupThreeApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.miniprojectgroupthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectGroupThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectGroupThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniproject/miniprojectgroupthree/MiniProjectGroupThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.miniprojectgroupthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectGroupThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
