8c6cbf4c306b192da8bdefd34c0f5144949973fe
mino <minho0719@a.ut.ac.kr>
2023-03-23 17:50:57 +0900

    chore : security, 비밀번호 암호화
    -  SecurityFilterChain
    - BCryptPasswordEncoder

---
+++ src/main/java/com/miniproject/miniprojectgroupthree/config/AuthenticationConfig.java
@@ -0,0 +1,46 @@
+package com.miniproject.miniprojectgroupthree.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class AuthenticationConfig {//spring security 설정
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("=============== confugure spring security filter chain==================");
+
+        http.csrf().disable();
+        http.httpBasic().disable();
+
+        return http.sessionManagement()
+                   .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
+
+    @Bean
+    protected WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("============= my configure=============");
+
+        return web -> {
+            web.ignoring().requestMatchers(PathRequest.toStaticResources()
+                            .atCommonLocations())
+                    .antMatchers("/api/*/users/join", "/api/*/users/login");
+        };
+    }
+
+}
