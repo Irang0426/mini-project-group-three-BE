e73d1178bbb6159432fcc8e927e2f211cb5cf171
mino <minho0719@a.ut.ac.kr>
2023-03-23 15:41:55 +0900

    feat : exception 처리
    - 에러 코드 클래스
    - restcontroller 예외처리

--- src/main/java/com/miniproject/miniprojectgroupthree/exception/AppException.java
+++ src/main/java/com/miniproject/miniprojectgroupthree/exception/AppException.java
@@ -1,5 +1,20 @@
 package com.miniproject.miniprojectgroupthree.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AppException extends RuntimeException {
 
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage() {
+        if (message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s , %s",errorCode.getMessage(),message);
+    }
 }
