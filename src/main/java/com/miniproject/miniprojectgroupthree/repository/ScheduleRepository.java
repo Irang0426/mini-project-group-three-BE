f829b9e182d3bbf3b707fae6726775b326715b11
Irang <irang0426@gmail.com>
2023-03-31 13:22:30 +0900

    [이상빈] fix: ScheduleRepository 코드 작성

--- src/main/java/com/miniproject/miniprojectgroupthree/repository/ScheduleRepository.java
+++ src/main/java/com/miniproject/miniprojectgroupthree/repository/ScheduleRepository.java
@@ -1,2 +1,7 @@
-package com.miniproject.miniprojectgroupthree.repository;public interface ScheduleRepository {
+package com.miniproject.miniprojectgroupthree.repository;
+
+import com.miniproject.miniprojectgroupthree.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
